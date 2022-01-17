aafd7268fc461f030b8d847706feda38387591f3
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-01-17 22:24:34 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "Color_mapping.h"
+#include "Test_pair.h"
+
 int main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
-
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
-
     return 0;
 }
