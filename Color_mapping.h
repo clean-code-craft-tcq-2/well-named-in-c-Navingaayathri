38170409de22e1a5d001a9424415e3dcd90f1059
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2022-01-17 22:24:10 +0530

    Update Color_mapping.h

--- Color_mapping.h
+++ Color_mapping.h
@@ -1,17 +1,5 @@
-#include <stdio.h>
-#include <assert.h>
+#include "declaration.h"
 
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-const int MAX_COLORPAIR_NAME_CHARS;
-
-typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
-
-void testNumberToPair( int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
-void testPairToNumber( enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
-int GetPairNumberFromColor(const ColorPair* colorPair);
-ColorPair GetColorFromPairNumber(int pairNumber);
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
